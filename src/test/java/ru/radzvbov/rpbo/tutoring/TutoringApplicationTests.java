12335eacc06d77ae360194816657480c65b500f6
Магомедсаид Раджабов <magomedsaidradzabov@MacBook-Pro-Magomedsaid.local>
2025-12-15 02:16:26 +0300

    Fix CI: move test config to test resources and disable SSL

--- src/test/java/ru/radzvbov/rpbo/tutoring/TutoringApplicationTests.java
+++ src/test/java/ru/radzvbov/rpbo/tutoring/TutoringApplicationTests.java
@@ -2,12 +2,13 @@ package ru.radzvbov.rpbo.tutoring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TutoringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
